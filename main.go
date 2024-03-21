ebd27fe3c57aad9f64d9164843fabac141804563
wisnu31899 <92870248+wisnu31899@users.noreply.github.com>
2024-03-21 16:02:32 +0700

    feat: multiple return

--- main.go
+++ main.go
@@ -3,16 +3,24 @@ package main
 import "fmt"
 
 //soal 1
-// func main() {
-// 	fmt.Printf("%.0f\n", pembulatan(4.37))  // Output: 4.4
-// 	fmt.Printf("%.1f\n", pembulatan(4.32))  // Output: 4.3
-// 	fmt.Printf("%.2f\n", pembulatan(4.324)) // Output: 4.30
-// 	fmt.Printf("%.3f\n", pembulatan(4.356)) // Output: 4.40
-// }
+func main() {
+	fmt.Println("Soal 1:")
+	val1, rem1 := pembulatan(4.37)
+	fmt.Printf("Nilai: %.1f, Sisa: %.1f\n", val1, rem1) // Output: 4.4, 0.0
+
+	val2, rem2 := pembulatan(4.32)
+	fmt.Printf("Nilai: %.1f, Sisa: %.1f\n", val2, rem2) // Output: 4.3, 0.0
+
+	val3, rem3 := pembulatan(4.324)
+	fmt.Printf("Nilai: %.2f, Sisa: %.2f\n", val3, rem3) // Output: 4.30, 0.0
+
+	val4, rem4 := pembulatan(4.356)
+	fmt.Printf("Nilai: %.3f, Sisa: %.3f\n", val4, rem4) // Output: 4.400, 0.0
+}
 
 // // soal 2
 // func main() {
-// 	deret := DeretBilangan(10)
+// 	deret := DeretBilangan(40)
 // 	deret.Prima()
 // 	deret.Ganjil()
 // 	deret.Genap()
@@ -20,30 +28,30 @@ import "fmt"
 // }
 
 // soal 3
-func main() {
-	ukuranBalok := Balok{
-		Panjang: 3.0,
-		Lebar:   4.0,
-		Tinggi:  5.0,
-	}
-	cetakInfo(ukuranBalok, "Balok")
+// func main() {
+// 	ukuranBalok := Balok{
+// 		Panjang: 3.0,
+// 		Lebar:   4.0,
+// 		Tinggi:  5.0,
+// 	}
+// 	cetakInfo(ukuranBalok, "Balok")
 
-	ukuranKubus := Kubus{
-		Sisi: 2.0,
-	}
-	cetakInfo(ukuranKubus, "Kubus")
+// 	ukuranKubus := Kubus{
+// 		Sisi: 2.0,
+// 	}
+// 	cetakInfo(ukuranKubus, "Kubus")
 
-	ukuranTabung := Tabung{
-		JariJari: 2.0,
-		Tinggi:   5.0,
-	}
-	cetakInfo(ukuranTabung, "Tabung")
-}
+// 	ukuranTabung := Tabung{
+// 		JariJari: 2.0,
+// 		Tinggi:   5.0,
+// 	}
+// 	cetakInfo(ukuranTabung, "Tabung")
+// }
 
-// Fungsi untuk mencetak informasi menggunakan interface BangunRuang
-func cetakInfo(b BangunRuang, nama string) {
-	fmt.Printf("==Rumus %s==\n", nama)
-	fmt.Printf("Luas: %.2f\n", b.luas())
-	fmt.Printf("Keliling: %.2f\n", b.keliling())
-	fmt.Printf("Volume: %.2f\n\n", b.volume())
-}
+// // Fungsi untuk mencetak informasi menggunakan interface BangunRuang
+// func cetakInfo(b BangunRuang, nama string) {
+// 	fmt.Printf("==Rumus %s==\n", nama)
+// 	fmt.Printf("Luas: %.2f\n", b.luas())
+// 	fmt.Printf("Keliling: %.2f\n", b.keliling())
+// 	fmt.Printf("Volume: %.2f\n\n", b.volume())
+// }
--- pembulatan.go
+++ pembulatan.go
@@ -4,7 +4,8 @@ import (
 	"math"
 )
 
-func pembulatan(nilai float64) float64 {
-	hasil := math.Round(nilai*10) / 10
-	return hasil
+func pembulatan(nilai float64) (float64, float64) {
+	rounded := math.Round(nilai*10) / 10
+	remainder := rounded - math.Floor(rounded)
+	return rounded, remainder
 }
